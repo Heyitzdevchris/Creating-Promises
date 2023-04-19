67e2f4820d302c36b46cdf9e6568d527ee10dfbf
Chris <chrisaaron184@gmail.com>
2023-04-18 19:15:41 -0700

    Update main.js file

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ fs.readFile("quotes.txt", (error, buffer) => {
   }
   const lines = buffer.toString().split("\n");
   quote = lines[Math.floor(Math.random() * lines.length)];
+
+  console.log(`Your quote is: ${quote}`);
 });
 
-console.log(`Your quote is: ${quote}`);
